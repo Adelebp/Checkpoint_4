11180e57d50d5e893178353e2c18bd5698e16184
Adèlebp <abobinparra@gmail.com>
2020-07-24 12:02:31 +0200

    add cv and toast

--- checkpoint_4/src/components/Cv.jsx
+++ checkpoint_4/src/components/Cv.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Toast, ToastBody, ToastHeader } from 'reactstrap';
 import styles from './Cv.module.css';
 
-export default function Cv() {
-    	return (
-            <div className={styles.container}>
+const Cv = (props) => {
+	const { buttonLabel } = props;
+	const [show, setShow] = useState(false);
 
-                </div>
-    	);
-    };
+	const toggle = () => setShow(!show);
+
+	return (
+		<div>
+			<h5 className={styles.h5}>Mon parcours</h5>
+
+			<div className='p-3 my-2 rounded'>
+				<Toast>
+					<ToastHeader>Reactstrap</ToastHeader>
+					<ToastBody>
+						This is a toast on a white background — check it out!
+					</ToastBody>
+				</Toast>
+			</div>
+		</div>
+	);
+};
+
+export default Cv;
